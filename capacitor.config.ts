e7d2d1641acd220c6fa087a48012ea013f13641f
David Jenkins <djenkins@hchb.com>
2022-08-12 17:36:49 -0400

    Adding capacitor and maps changes

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.djenkins.capacitorapp',
+  appName: 'capacitor-app',
+  webDir: 'dist/capacitor-app',
+  bundledWebRuntime: false
+};
+
+export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GoogleMap } from '@capacitor/google-maps';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'capacitor-app';
+
+  @ViewChild('map') mapRef: ElementRef<HTMLElement> | undefined;
+  map: GoogleMap | undefined;
+
+  ngAfterViewInit() {
+		setTimeout(() => {
+      this.createMap();
+    }, 1000);
+	}
+
+  async createMap() {
+    if (this.mapRef == null) {
+      console.info("mapRef null")
+      return;
+    }
+    console.info("mapRef not null")
+
+    this.map = await GoogleMap.create({
+      id: 'capacitor-google-maps',
+      element: this.mapRef.nativeElement,
+      apiKey: environment.apiKey,
+      config: {
+        center: {
+          lat: 33.6,
+          lng: -117.9,
+        },
+        zoom: 8,
+      },
+    });
+  }
 }
